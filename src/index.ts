26863805094f6dd062701655614229d63e0fae6f
Dylan DesRosier <dylan@pooltogether.com>
2021-09-28 11:25:01 -0400

    expose helpers

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
+// Core functions
 export * from './tsunamiDrawCalculator';
 export * from './prepareClaims';
+
+// Types
 export * from './types'
+
+// Helpers
 export * from './computeDrawResults'
 export * from './computePicks'
 export * from './generatePicks'
+export * from './helpers/calculateNumberOfPrizesForIndex'
+export * from './helpers/calculatePrizeForDistributionIndex'
