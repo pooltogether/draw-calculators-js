c089ced953318040a2a3a6ea52656ae75dce8451
kamescg <kamescg@gmail.com>
2021-09-29 16:01:25 -0400

    check

--- src/prepareClaims.ts
+++ src/prepareClaims.ts
@@ -2,27 +2,27 @@ import { BigNumber } from "ethers"
 import { User, DrawResults, Claim } from "./types"
 
 // for a given User and DrawResult, prepares the input for the contract ClaimableDraw::claim() call
-export function prepareClaims(user: User, drawResults: DrawResults[]) : Claim {
-    let claim : Claim = {
+export function prepareClaims(user: User, drawResults: DrawResults[]): Claim {
+    let claim: Claim = {
         userAddress: user.address,
         drawIds: [],
         data: []
     }
-    if(drawResults.length == 0) {
+    if (drawResults.length == 0) {
         return claim
     }
     //for each draw run the runDrawCalculatorForSingleDraw, if there is a winner add to result
     drawResults.forEach(drawResult => {
-        if(drawResult.totalValue.gt(BigNumber.from(0))){
+        if (drawResult?.totalValue.gt(BigNumber.from(0))) {
             claim.drawIds.push(drawResult.drawId)
             // now add the pickIndices data
             let winningPicks: BigNumber[] = []
-            for(const prizeAwardable of drawResult.prizes){
+            for (const prizeAwardable of drawResult.prizes) {
                 winningPicks.push(prizeAwardable.pick)
             }
-            claim.data.push(winningPicks) 
+            claim.data.push(winningPicks)
         }
-    }); 
+    });
     return claim
 
 }
