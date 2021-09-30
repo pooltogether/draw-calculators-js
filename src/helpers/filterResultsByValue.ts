0c67eac6396becc8ef474ee5049ce01357a3f3d0
Dylan DesRosier <dylan@pooltogether.com>
2021-09-30 15:53:07 -0400

    update filter fn

--- src/helpers/filterResultsByValue.ts
+++ src/helpers/filterResultsByValue.ts
@@ -1,32 +1,50 @@
-import { BigNumber } from "ethers";
-import { DrawResults, PrizeAwardable } from "types";
+import { BigNumber } from 'ethers';
+import { DrawResults, PrizeAwardable } from 'types';
 
-const debug = require('debug')('pt:tsunami-sdk-drawCalculator')
+const debug = require('debug')('pt:tsunami-sdk-drawCalculator');
 
-export function filterResultsByValue(drawResults: DrawResults, maxPicksPerUser: number) : DrawResults{
-    // if the user has more winning picks than max pick per user for the draw, we sort by value and remove the lowest value picks
-    if(drawResults.prizes.length > maxPicksPerUser){
-        
-        debug(`user has more claims (${drawResults.prizes.length}) than the max picks per user (${maxPicksPerUser}). Sorting..`)
-        // sort by value
-        const descendingSortedPrizes : PrizeAwardable[]= drawResults.prizes.sort(
-            function(a : PrizeAwardable, b: PrizeAwardable) : number {
-                const subbedValue = a.amount.sub(b.amount)
-                if (subbedValue.isZero()) return 0
-                if (subbedValue.isNegative()) return -1
-                return 1
-            })
-        // remove the lowest value picks up to the max picks per user
-        const sortedDescendingSortedPrizes = descendingSortedPrizes.slice(0, maxPicksPerUser).filter(prizeAwardable => !prizeAwardable.amount.isZero())
-        // sum the sorted values
-        const newTotalValue : BigNumber = descendingSortedPrizes.reduce((accumulator, currentValue) => accumulator.add(currentValue.amount), BigNumber.from(0))
-        return {
-            ...drawResults,
-            totalValue: newTotalValue,
-            prizes: sortedDescendingSortedPrizes
-        } 
+/**
+ * Filters out prizes if:
+ * - there's more prizes than the max picks per user
+ * - the prize won is 0 tokens
+ *
+ * Sorts prizes by descending value too.
+ * @param drawResults
+ * @param maxPicksPerUser
+ * @returns
+ */
+export function filterResultsByValue(
+    drawResults: DrawResults,
+    maxPicksPerUser: number,
+): DrawResults {
+    // sort by value
+    let newPrizes = drawResults.prizes.filter(filterZeroPrizeAmount).sort(sortByPrizeAmount);
+    let newTotalValue = drawResults.totalValue;
+
+    // If there's too many prizes to claim, slice & sum total value
+    if (drawResults.prizes.length > maxPicksPerUser) {
+        debug(
+            `user has more claims (${drawResults.prizes.length}) than the max picks per user (${maxPicksPerUser}). Sorting..`,
+        );
+        newPrizes = newPrizes.slice(0, maxPicksPerUser);
+        newTotalValue = newPrizes.reduce(
+            (accumulator, currentValue) => accumulator.add(currentValue.amount),
+            BigNumber.from(0),
+        );
     }
 
-    // if not greater than max picks per user, return the whole array
-    return drawResults
+    return {
+        ...drawResults,
+        totalValue: newTotalValue,
+        prizes: newPrizes,
+    };
 }
+
+const sortByPrizeAmount = (a: PrizeAwardable, b: PrizeAwardable) => {
+    const subbedValue = a.amount.sub(b.amount);
+    if (subbedValue.isZero()) return 0;
+    if (subbedValue.isNegative()) return -1;
+    return 1;
+};
+
+const filterZeroPrizeAmount = (prizeAwardable: PrizeAwardable) => !prizeAwardable.amount.isZero();
