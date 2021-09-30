b64f6b8f2cc3d80ad1e5bc43e3bf06255ad3feaa
Dylan DesRosier <dylan@pooltogether.com>
2021-09-30 12:06:33 -0400

    filter out $0 prizes

--- src/helpers/filterResultsByValue.ts
+++ src/helpers/filterResultsByValue.ts
@@ -17,7 +17,7 @@ export function filterResultsByValue(drawResults: DrawResults, maxPicksPerUser: 
                 return 1
             })
         // remove the lowest value picks up to the max picks per user
-        const sortedDescendingSortedPrizes = descendingSortedPrizes.slice(0, maxPicksPerUser)
+        const sortedDescendingSortedPrizes = descendingSortedPrizes.slice(0, maxPicksPerUser).filter(prizeAwardable => !prizeAwardable.amount.isZero())
         // sum the sorted values
         const newTotalValue : BigNumber = descendingSortedPrizes.reduce((accumulator, currentValue) => accumulator.add(currentValue.amount), BigNumber.from(0))
         return {
